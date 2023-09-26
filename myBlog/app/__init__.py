60e021228cd09c4a56502dcd0f80fb2112930f3b
Jonathan Nguyen <jonathanguven@gmail.com>
2023-09-26 02:40:42 -0700

    completed slides tutorial

--- myBlog/app/__init__.py
+++ myBlog/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, flash, redirect, request, render_template, session, abort
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = 'sup'
+)
 
 from app import routes
--- myBlog/app/routes.py
+++ myBlog/app/routes.py
@@ -1,4 +1,5 @@
-from app import app, render_template
+from app import app, render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route("/")
 def hello():
@@ -14,3 +15,14 @@ def hello():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'
+            .format(form.username.data, form.remember_me.data))
+        return redirect('/home')
+    return render_template('login.html', title='Sign In', form=form)
+
+
