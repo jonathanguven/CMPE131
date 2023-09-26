f7c18709e7a7f865f80264699f11900be81d9986
Jonathan Nguyen <jonathanguven@gmail.com>
2023-09-26 02:57:58 -0700

    fixed flash error

--- myBlog/app/routes.py
+++ myBlog/app/routes.py
@@ -2,6 +2,7 @@ from app import app, render_template, flash, redirect
 from .forms import LoginForm
 
 @app.route("/")
+@app.route('/index')
 def hello():
     user = {'username': 'Jonathan'}
     posts = [
@@ -20,9 +21,10 @@ def hello():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        print('validated')
         flash('Login requested for user {}, remember_me={}'
             .format(form.username.data, form.remember_me.data))
-        return redirect('/home')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 
