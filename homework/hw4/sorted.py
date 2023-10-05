a62c56c11c83904fe97eccbc377533b63b77b03b
Jonathan Nguyen <jonathanguven@gmail.com>
2023-10-05 11:43:12 -0700

    completed q3

--- homework/hw4/sorted.py
+++ homework/hw4/sorted.py
@@ -1,5 +1,9 @@
-def sort_dictionary(dict):
-    sorted(dict.keys(), reverse=True)
-    return [(name, dict[name][0]) for name in dict]
+def sort_dictionary(dictionary):
+    if not isinstance(dictionary, dict):
+        return "input is not a valid dictionary"
+    sorted(dictionary.keys(), reverse=True)
+    return [(name, dictionary[name][0]) for name in dictionary]
 
 print(sort_dictionary({'Tom' : (5464512, 24) , 'Sara' : (5446987, 32) , 'Mary' : (1557896, 20)}))
+print(sort_dictionary(23))
+print(sort_dictionary([1, 2, 3, 4, 5]))
